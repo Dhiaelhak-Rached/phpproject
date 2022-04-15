7f46de5826e3c2834f802789a187a6169481dfe6
DhiaelhakRached <75325276+Kiraxer@users.noreply.github.com>
2022-04-15 02:17:22 +0100

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,57 @@
+<?php session_start(); ?>
+<?php include('dbcon.php'); ?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style1.css">
+
+</head>
+<body>
+<div class="form-wrapper">
+  
+  <form action="#" method="post">
+    <h3>Login here</h3>
+	
+    <div class="form-item">
+		<input type="text" name="user" required="required" placeholder="Username" autofocus required></input>
+    </div>
+    
+    <div class="form-item">
+		<input type="password" name="pass" required="required" placeholder="Password" required></input>
+    </div>
+    
+    <div class="button-panel">
+		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
+    </div>
+  </form>
+  <?php
+	if (isset($_POST['login']))
+		{
+			$username = mysqli_real_escape_string($con, $_POST['user']);
+			$password = mysqli_real_escape_string($con, $_POST['pass']);
+			
+			$query 		= mysqli_query($con, "SELECT * FROM user WHERE  pwd='$password' and email='$username'");
+			$row		= mysqli_fetch_array($query);
+			$num_row 	= mysqli_num_rows($query);
+			
+			if ($num_row > 0) 
+				{			
+					$_SESSION['user_id']=$row['user_id'];
+					header('location:../Front/index.html');
+					
+				}
+			else
+				{
+					echo 'Invalid Username and Password Combination';
+				}
+		}
+  ?>
+  <div class="reminder">
+    <p>Not a member? <a href="../ajouter.php">Sign up now</a></p>
+	<p>Not a member? <a href="../ajouteradmin.php">Sign up now(Admin)</a></p>
+    <p><a href="#">Forgot password?</a></p>
+  </div>
+  
+</div>
+
+</body>
+</html>
